c5bf01f14f30419d53edb1ee6983d8769712dc00
Kyra Mason-Salmon <k.mason-salmon@se18.qmul.ac.uk>
2020-04-13 11:26:56 +0100

    Login: Edit2

--- app/src/main/java/com/example/fitness_application/MainActivity.java
+++ app/src/main/java/com/example/fitness_application/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.fitness_application;
-
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         final Button register = findViewById(R.id.bregister);
         final EditText email = findViewById(R.id.email);
         final EditText password = findViewById(R.id.password);
+        mAuth = FirebaseAuth.getInstance();
 
         register.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -29,39 +40,95 @@ public class MainActivity extends AppCompatActivity {
         login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                validateCredentials(email,password);
+                validateCredentials(email,password,true);
             }
         });
     }
 
-    public void validateCredentials(EditText email,EditText password){
+    public void onStart() {
+        super.onStart();
+        // Check if user is signed in (non-null) and update UI accordingly.
+        FirebaseUser currentUser = mAuth.getCurrentUser();
+        updateUI(currentUser);
+    }
+
+    private void updateUI(FirebaseUser currentUser) {
+        if(currentUser == null){
+            //inform them.
+            showAlertDialogButtonClicked();
+        }
+    }
+
+    public void showAlertDialogButtonClicked() {
+        // setup the alert builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Login Failed");
+        builder.setMessage("Either your email, password, or both are incorrect.");
+
+        // add a button
+        builder.setPositiveButton("OK", null);
+
+        // create and show the alert dialog
+        AlertDialog dialog = builder.create();
+        dialog.show();
+    }
+
+    public void validateCredentials(EditText email,EditText password, Boolean isValid){
         String emailText = email.getText().toString().trim();
         String passwordText = password.getText().toString().trim();
 
         if(emailText.equals("")){
             email.setError("Please input your email");
-            return;
+            isValid = false;
         }
         if(passwordText.equals("")){
             password.setError("Please input your password");
-            return;
+            isValid = false;
         }
 
         if(!(emailText.contains("@")) || emailText.length() < 10  || emailText.length() > 40){
             email.setError("Please input a valid email");
-            return;
+            isValid = false;
         }
 
         if(passwordText.length() < 5 || passwordText.length() > 20){
             password.setError("Please input a valid password");
-            return;
+            isValid = false;
         }
+
+        if(isValid){
+            //check credentials against database.
+            credentialsInDatabase(emailText,passwordText);
+        }
+
+        return;
 
     }
 
 // Database code
-    public boolean credentialsInDatabase(String emailText, String passwordText){
+    public void credentialsInDatabase(String emailText, String passwordText){
+        mAuth.signInWithEmailAndPassword(emailText, passwordText)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    private static final String TAG = "";
 
-        return false;
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, update UI with the signed-in user's information
+                            Log.d(TAG, "signInWithEmail:success");
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            updateUI(user);
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Log.w(TAG, "signInWithEmail:failure", task.getException());
+                            //Toast.makeText(EmailPasswordActivity.this, "Authentication failed.",
+                                    //Toast.LENGTH_SHORT).show();
+                            updateUI(null);
+                        }
+
+                        // ...
+                    }
+                });
+
     }
 }
